43b2a9da400553373479b03d2a4bd020ac1df9a3
Kyryu <lolnhoxvtpn@gmail.com>
2021-11-26 10:11:49 +0700

    10h11 - 26.11.2021

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from 'react'
 import * as Realm from "realm-web";
+import * as bootstrap from 'bootstrap';
 import _ from 'lodash'
 import {Helmet} from 'react-helmet';
 
