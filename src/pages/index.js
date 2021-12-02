1115d8b8182e8838a63b1ddc4abd8a098642de83
Kyryu <lolnhoxvtpn@gmail.com>
2021-12-02 08:39:43 +0700

    05h39 - 02.11.2021

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,51 @@ function Index(props){
                 <title>{props.value}</title>
                 <meta name="description" content="Đội xe Hùng Hậu"/>
             </Helmet>
+            <div className="d-flex flex-column flex-shrink-0" style={{"width": "4.5rem", "height": "100vh", "background-color":"rgba(0, 17, 102, 0.8)"}}>
+                <a href="/" className="d-block p-3 link-dark text-decoration-none" title="HungHau Holdings" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <img src="./favicon.ico" width="40"/>
+                    <span className="visually-hidden">Icon-only</span>
+                </a>
+                <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link active py-3 border-bottom text-white" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+                            <i className="bi bi-house-door"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link py-3 border-bottom text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                            <i className="bi bi-speedometer2"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link py-3 border-bottom text-white" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+                            <i className="bi bi-table"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link py-3 border-bottom text-white" title="Products" data-bs-toggle="tooltip" data-bs-placement="right">
+                            <i className="bi bi-grid"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link py-3 border-bottom text-white" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
+                            <i className="bi bi-person-circle"></i>
+                        </a>
+                    </li>
+                </ul>
+                <div className="dropdown border-top">
+                    <a href="#" className="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle text-white" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" className="rounded-circle" />
+                    </a>
+                    <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+                        <li><a className="dropdown-item" href="#">New project...</a></li>
+                        <li><a className="dropdown-item" href="#">Settings</a></li>
+                        <li><a className="dropdown-item" href="#">Profile</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -17,50 +17,52 @@ function LogIn(props){
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(user.toLowerCase() === 'ad@hunghau.vn' && pass === 'Bb123456'){
+        if(user.toLowerCase() === 'ad@hunghau.vn' && pass === 'HH@@2016'){
             window.location.href = '/home';
         }else{
             var trigger = document.getElementById('falseLoginToast');
             var toast = new bootstrap.Toast(trigger);
             toast.show();
+            setUser("");
+            setPass("");
         }
     }
 
     return(
-        <div class="signin">
+        <div className="signin">
             <Helmet titleTemplate="%s | Quản lý Đội xe Hùng Hậu">
                 <title>{props.value}</title>
                 <meta name="description" content="Đội xe Hùng Hậu"/>
             </Helmet>
-            <div class="text-center">
-                <div class="form-signin">
+            <div className="t-center text-center bg-hunghau">
+                <div className="form-signin">
                     <form onSubmit={handleSubmit}>
-                        <img class="mb-4" src="./favicon.ico" alt="" width="100" />
-                        <h1 class="mb-3"><b>Đăng nhập</b></h1>
+                        <img className="mb-4" src="./favicon.ico" alt="" width="100" />
+                        <h1 className="mb-3"><b>Đăng nhập</b></h1>
 
-                        <div class="form-floating mb-2">
-                            <input type="text" class="form-control" id="ftun" placeholder="Tài khoản" required value={user} onChange={(e) => setUser(e.target.value)} />
+                        <div className="form-floating mb-2">
+                            <input type="text" className="form-control" id="ftun" placeholder="Tài khoản" required value={user} onChange={(e) => setUser(e.target.value)} />
                             <label for="ftun" style={{"color": "black"}}>Tài khoản</label>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="flpw" placeholder="Mật khẩu" required value={pass} onChange={(e) => setPass(e.target.value)}/>
+                        <div className="form-floating mb-3">
+                            <input type="password" className="form-control" id="flpw" placeholder="Mật khẩu" required value={pass} onChange={(e) => setPass(e.target.value)}/>
                             <label for="flpw" style={{"color": "black"}}>Mật khẩu</label>
                         </div>
-                        <div class="checkbox mb-3">
+                        <div className="checkbox mb-3">
                             <input type="checkbox" value="remember-me" /> Ghi nhớ đăng nhập
                         </div>
-                        <button class="w-100 btn btn-lg btn-primary" id="loginBtn" type="submit">Đăng nhập</button>
-                        <p class="mt-5">Copyright 2021 &copy; <b>HungHau Holding</b></p>
+                        <button className="w-100 btn btn-lg btn-primary" id="loginBtn" type="submit">Đăng nhập</button>
+                        <p className="mt-5">Copyright 2021 &copy; <b>HungHau Holding</b></p>
                     </form>
-                    <div class="toast-container position-fixed top-0 end-0 p-3" id="notifications" style={{"z-index": "11"}}>
-                        <div id="falseLoginToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <img src="./favicon.ico" class="rounded me-2" alt="..." style={{"height":"25px"}} />
-                                <strong class="me-auto">Lỗi đăng nhập</strong>
+                    <div className="toast-container position-fixed top-0 end-0 p-3" id="notifications" style={{"z-index": "11"}}>
+                        <div id="falseLoginToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                            <div className="toast-header">
+                                <img src="./favicon.ico" className="rounded me-2" alt="..." style={{"height":"25px"}} />
+                                <strong className="me-auto">Lỗi đăng nhập</strong>
                                 <small>Vài giây trước</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                                <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                             </div>
-                            <div class="toast-body bg-primary">
+                            <div className="toast-body bg-primary">
                                 Sai tài khoản hoặc mật khẩu, vui lòng kiểm tra lại!
                             </div>
                         </div>
